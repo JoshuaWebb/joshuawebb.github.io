b455511bb84ff2660e265a1ecdc020bd9ab74fb4
Joshua Webb <bbew.auhsoj+git@gmail.com>
2017-02-01 21:46:28 +1100

    Wait for all images to load before animation

--- scripts/main.js
+++ scripts/main.js
@@ -32,14 +32,31 @@ function DOMContentLoaded() {
     initColor();
     fetchJSONFile('data/links.json', function(data){
         var list = document.getElementById("links");
-        var newItems = [];
+        var loaded = 0;
+        var numLinks = data.length;
+
+        // Wait for all images to load before animating
+        function imageLoaded() {
+            loaded++;
+            if (loaded == numLinks) {
+                list.className += " loaded";
+            }
+        }
+
         data.forEach(function(item, index) {
+            // Load the icon as an Image so we can get a callback
+            // (even if it is loaded from cache)
+            var image = new Image();
+            image.onload = imageLoaded;
+            image.src = item.iconUrl;
+
             var newItem = document.createElement('li');
             var newLink = document.createElement('a');
             newLink.title = item.title;
             newLink.style.backgroundImage = 'url(' + item.iconUrl + ')';
             newLink.href = item.linkUrl;
             newLink.style["-webkit-animation-delay"] = index * 0.1 + 's';
+
             newItem.appendChild(newLink);
             list.appendChild(newItem);
         });
